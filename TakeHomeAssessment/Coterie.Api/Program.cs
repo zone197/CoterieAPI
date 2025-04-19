de368cbf6364a2313fb724f34e30c9a932311bcd
zone197 <megjoseph7@gmail.com>
2025-04-19 14:58:16 -0400

    Initial commit

---
+++ TakeHomeAssessment/Coterie.Api/Program.cs
@@ -0,0 +1,33 @@
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure NLog
+builder.Logging.ClearProviders();
+builder.Logging.AddNLog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
